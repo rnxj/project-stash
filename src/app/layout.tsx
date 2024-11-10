49b310bad65c9c2c62d63ba6d79e9401f8012688
Reuel Nixon <reuelnixon@gmail.com>
2024-11-10 13:09:34 +0530

    feat: vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 
@@ -39,6 +40,7 @@ export default function RootLayout({
             <Toaster />
           </WalletAdapterProvider>
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
